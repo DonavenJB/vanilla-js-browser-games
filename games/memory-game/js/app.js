dc4134a85f6ad6b3cf017dc6e07ef74dcd42c123
Donaven Bruce <91450415+DonavenJB@users.noreply.github.com>
2022-06-24 20:12:00 -0700

    Improve memory game matching

--- games/memory-game/js/app.js
+++ games/memory-game/js/app.js
@@ -48,65 +48,87 @@ const cardArray = [
         img: 'images/BlackRook.png'
     }
 ]
+
+const CARD_BACK = 'images/space.jpeg'
+const MATCHED_CARD = 'images/favicon.jpg'
+
 cardArray.sort(() => 0.5 - Math.random())
 
 const gridDisplay = document.querySelector('#grid')
-const reslutDisplay = document.querySelector('#result')
+const resultDisplay = document.querySelector('#result')
+
 let cardsChosen = []
 let cardsChosenIds = []
+let boardLocked = false
+
 const cardsWon = []
 
-function createBoard () {
+function createBoard() {
     for (let i = 0; i < cardArray.length; i++) {
         const card = document.createElement('img')
-        card.setAttribute('src', 'images/favicon.jpg')
+
+        card.setAttribute('src', CARD_BACK)
         card.setAttribute('data-id', i)
+        card.setAttribute('alt', 'Hidden memory card')
+
         card.addEventListener('click', flipCard)
+
         gridDisplay.appendChild(card)
     }
+
+    resultDisplay.textContent = '0'
 }
-createBoard()
 
 function checkMatch() {
-   const cards = document.querySelectorAll('img')
-   const optionOneId = cardsChosenIds[0]
-   const optionTwoId = cardsChosenIds[1]
-    console.log(cards)
-    console.log('check for match')
-    if (optionOneId == optionTwoId) {
-        alert('you have clicked the same image!')
-    }
-    if (cardsChosen[0] == cardsChosen[1]) {
-        alert('you found a match!')
-        cards[optionOneId].setAttribute('src', 'images/favicon.jpg')
-        cards[optionTwoId].setAttribute('src', 'images/favicon.jpg')
+    const cards = document.querySelectorAll('#grid img')
+
+    const optionOneId = Number(cardsChosenIds[0])
+    const optionTwoId = Number(cardsChosenIds[1])
+
+    if (cardsChosen[0] === cardsChosen[1]) {
+        cards[optionOneId].setAttribute('src', MATCHED_CARD)
+        cards[optionTwoId].setAttribute('src', MATCHED_CARD)
+
         cards[optionOneId].removeEventListener('click', flipCard)
         cards[optionTwoId].removeEventListener('click', flipCard)
+
         cardsWon.push(cardsChosen)
     } else {
-        cards[optionOneId].setAttribute('src', 'images/space.jpeg')
-        cards[optionTwoId].setAttribute('src', 'images/space.jpeg')
-        alert('sorry try again!')
+        cards[optionOneId].setAttribute('src', CARD_BACK)
+        cards[optionTwoId].setAttribute('src', CARD_BACK)
     }
-    reslutDisplay.innerHTML = cardsWon.length
+
+    resultDisplay.textContent = cardsWon.length
 
     cardsChosen = []
     cardsChosenIds = []
+    boardLocked = false
 
-    if (cardsWon.length == cardArray.length/2) {
-        reslutDisplay.innerHTML = ' YOU FOUND THEM ALL'
-
+    if (cardsWon.length === cardArray.length / 2) {
+        resultDisplay.textContent = 'YOU FOUND THEM ALL'
     }
 }
 
 function flipCard() {
-    const cardId = this.getAttribute('data-id')
+    if (boardLocked) {
+        return
+    }
+
+    const cardId = Number(this.getAttribute('data-id'))
+
+    if (cardsChosenIds.includes(cardId)) {
+        return
+    }
+
     cardsChosen.push(cardArray[cardId].name)
     cardsChosenIds.push(cardId)
-    console.log(cardsChosen)
-    console.log(cardsChosenIds)
+
     this.setAttribute('src', cardArray[cardId].img)
+
     if (cardsChosen.length === 2) {
-        setTimeout( checkMatch, 500)
+        boardLocked = true
+        setTimeout(checkMatch, 650)
     }
 }
+
+createBoard()
