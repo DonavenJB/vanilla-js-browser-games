8a8267b228ffcd9e15b04a8dc615c57ec17b251a
Donaven Bruce <91450415+DonavenJB@users.noreply.github.com>
2022-07-04 19:16:00 -0700

    Improve card game round flow

--- games/card-game/js/main.js
+++ games/card-game/js/main.js
@@ -1,40 +1,117 @@
 let deckId = ''
+let drawing = false
 
-fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
-    .then(res => res.json())
-    .then(data => {
-        deckId = data.deck_id
-    })
-    .catch(err => {
-        console.log(`error ${err}`)
-    })
+const drawButton = document.querySelector('#draw-button')
+const player1Card = document.querySelector('#player1')
+const player2Card = document.querySelector('#player2')
+const resultDisplay = document.querySelector('#result')
+const statusDisplay = document.querySelector('#game-status')
 
-document.querySelector('button').addEventListener('click', getFetch)
+function createDeck() {
+    drawing = true
+    drawButton.disabled = true
 
-function getFetch() {
-    const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`
+    statusDisplay.textContent = 'Shuffling deck...'
 
-    fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-
-            const val1 = Number(cardValue(data.cards[0].value))
-            const val2 = Number(cardValue(data.cards[1].value))
-
-            document.querySelector('#player1').src = data.cards[0].image
-            document.querySelector('#player2').src = data.cards[1].image
-
-            if (val1 > val2) {
-                document.querySelector('h3').innerText = 'player 1 WON!'
-            } else if (val1 < val2) {
-                document.querySelector('h3').innerText = 'player 2 won'
-            } else {
-                document.querySelector('h3').innerText = 'WAR'
+    fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+        .then(res => {
+            if (!res.ok) {
+                throw new Error('Unable to reach card service.')
             }
+
+            return res.json()
+        })
+        .then(data => {
+            if (!data.success) {
+                throw new Error('Unable to create deck.')
+            }
+
+            deckId = data.deck_id
+
+            statusDisplay.textContent = 'Ready'
+            resultDisplay.textContent = 'Waiting for the first draw.'
+
+            drawButton.disabled = false
         })
         .catch(err => {
-            console.log(`error ${err}`)
+            console.error(err)
+
+            deckId = ''
+
+            statusDisplay.textContent = 'Unable to load deck'
+            resultDisplay.textContent = 'Please refresh and try again.'
+        })
+        .finally(() => {
+            drawing = false
+        })
+}
+
+function drawCards() {
+    if (drawing || !deckId) {
+        return
+    }
+
+    drawing = true
+    drawButton.disabled = true
+
+    statusDisplay.textContent = 'Drawing...'
+
+    const url =
+        `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`
+
+    fetch(url)
+        .then(res => {
+            if (!res.ok) {
+                throw new Error('Unable to reach card service.')
+            }
+
+            return res.json()
+        })
+        .then(data => {
+            if (!data.success || data.cards.length < 2) {
+                throw new Error('Unable to draw two cards.')
+            }
+
+            const player1 = data.cards[0]
+            const player2 = data.cards[1]
+
+            const val1 = cardValue(player1.value)
+            const val2 = cardValue(player2.value)
+
+            player1Card.src = player1.image
+            player2Card.src = player2.image
+
+            player1Card.alt =
+                `${player1.value} of ${player1.suit}`
+
+            player2Card.alt =
+                `${player2.value} of ${player2.suit}`
+
+            if (val1 > val2) {
+                resultDisplay.textContent = 'Player 1 wins!'
+            } else if (val1 < val2) {
+                resultDisplay.textContent = 'Player 2 wins!'
+            } else {
+                resultDisplay.textContent =
+                    'WAR! Same rank - draw again.'
+            }
+
+            statusDisplay.textContent = 'Round complete'
+        })
+        .catch(err => {
+            console.error(err)
+
+            statusDisplay.textContent = 'Draw failed'
+
+            resultDisplay.textContent =
+                'Something went wrong. Try again.'
+        })
+        .finally(() => {
+            drawing = false
+
+            if (deckId) {
+                drawButton.disabled = false
+            }
         })
 }
 
@@ -48,3 +125,7 @@ function cardValue(value) {
 
     return faceValues[value.toUpperCase()] || Number(value)
 }
+
+drawButton.addEventListener('click', drawCards)
+
+createDeck()
