05f9bba77a2f2d080c8a61bb7d8ade72a52363dc
Donaven Bruce <91450415+DonavenJB@users.noreply.github.com>
2022-10-22 19:36:00 -0700

    Add Rock Paper Scissors session scoring

--- games/rock-paper-scissors/js/app.js
+++ games/rock-paper-scissors/js/app.js
@@ -1,17 +1,42 @@
 const computerChoiceDisplay = document.getElementById('computer-choice')
 const userChoiceDisplay = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('result')
+
+const userWinsDisplay =
+    document.getElementById('user-wins')
+
+const computerWinsDisplay =
+    document.getElementById('computer-wins')
+
+const drawsDisplay =
+    document.getElementById('draws')
+
 const possibleChoices = document.querySelectorAll('button')
 const computerChoices = ['rock', 'paper', 'scissors']
 
 let userChoice
 let computerChoice
 
+let userWins = 0
+let computerWins = 0
+let draws = 0
+
 function formatChoice(choice) {
     return (
         choice.charAt(0).toUpperCase() +
         choice.slice(1)
     )
+}
+
+function updateSessionScore() {
+    userWinsDisplay.textContent =
+        userWins
+
+    computerWinsDisplay.textContent =
+        computerWins
+
+    drawsDisplay.textContent =
+        draws
 }
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
@@ -47,31 +72,45 @@ function getResult() {
         formatChoice(computerChoice)
 
     if (computerChoice === userChoice) {
+        draws++
+
         result =
             `${formattedUserChoice} meets ${formattedComputerChoice} — it's a draw!`
     } else if (
         userChoice === 'rock' &&
         computerChoice === 'scissors'
     ) {
+        userWins++
+
         result =
             `${formattedUserChoice} beats ${formattedComputerChoice} — you win!`
     } else if (
         userChoice === 'paper' &&
         computerChoice === 'rock'
     ) {
+        userWins++
+
         result =
             `${formattedUserChoice} beats ${formattedComputerChoice} — you win!`
     } else if (
         userChoice === 'scissors' &&
         computerChoice === 'paper'
     ) {
+        userWins++
+
         result =
             `${formattedUserChoice} beats ${formattedComputerChoice} — you win!`
     } else {
+        computerWins++
+
         result =
             `${formattedComputerChoice} beats ${formattedUserChoice} — you lose!`
     }
 
     resultDisplay.textContent =
         result
+
+    updateSessionScore()
 }
+
+updateSessionScore()
