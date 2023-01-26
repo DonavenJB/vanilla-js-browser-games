2f6ac62132eac716e021448c32ea53dec3d44ec2
Donaven Bruce <91450415+DonavenJB@users.noreply.github.com>
2023-01-25 19:39:00 -0800

    Refactor Frogger movement and lifecycle

--- games/frogger/game.js
+++ games/frogger/game.js
@@ -1,332 +1,560 @@
-const timeLeftDisplay = document.querySelector('#time-left')
-const resultDisplay = document.querySelector('#result')
-const startPauseButton = document.querySelector('#start-pause-button')
-const resetSessionButton = document.querySelector('#reset-session-button')
-const winsDisplay = document.querySelector('#wins')
-const lossesDisplay = document.querySelector('#losses')
-const gamesPlayedDisplay = document.querySelector('#games-played')
-const bestTimeDisplay = document.querySelector('#best-time')
-const gameStatusDisplay = document.querySelector('#game-status')
-const squares = document.querySelectorAll('.grid div')
-const logsLeft = document.querySelectorAll('.log-left')
-const logsRight = document.querySelectorAll('.log-right')
-const carsLeft = document.querySelectorAll('.car-left')
-const carsRight = document.querySelectorAll('.car-right')
-const initialSquareClasses = Array.from(squares, square => square.className)
+import {
+  GameState
+} from '../../shared/js/game-state.js'
 
-const startingIndex = Array.from(squares).findIndex(square => square.classList.contains('starting-block'))
-let currentIndex = startingIndex
+import {
+  createTimerRegistry
+} from '../../shared/js/timers.js'
+
+const timeLeftDisplay =
+  document.querySelector('#time-left')
+
+const resultDisplay =
+  document.querySelector('#result')
+
+const startPauseButton =
+  document.querySelector('#start-pause-button')
+
+const resetSessionButton =
+  document.querySelector('#reset-session-button')
+
+const winsDisplay =
+  document.querySelector('#wins')
+
+const lossesDisplay =
+  document.querySelector('#losses')
+
+const gamesPlayedDisplay =
+  document.querySelector('#games-played')
+
+const bestTimeDisplay =
+  document.querySelector('#best-time')
+
+const gameStatusDisplay =
+  document.querySelector('#game-status')
+
+const squares =
+  document.querySelectorAll('.grid div')
+
+const logsLeft =
+  document.querySelectorAll('.log-left')
+
+const logsRight =
+  document.querySelectorAll('.log-right')
+
+const carsLeft =
+  document.querySelectorAll('.car-left')
+
+const carsRight =
+  document.querySelectorAll('.car-right')
+
+const initialSquareClasses =
+  Array.from(
+    squares,
+    square => square.className
+  )
+
+const startingIndex =
+  Array.from(squares)
+    .findIndex(square =>
+      square.classList.contains(
+        'starting-block'
+      )
+    )
+
 const width = 9
-let timerId
-let outcomeTimerId
-let currentTime = 20
-let gameOver = false
+const startingTime = 20
+
+const timers =
+  createTimerRegistry()
+
+const logCycle = [
+  'l1',
+  'l2',
+  'l3',
+  'l4',
+  'l5'
+]
+
+const carCycle = [
+  'c1',
+  'c2',
+  'c3'
+]
+
+let currentIndex =
+  startingIndex
+
+let currentTime =
+  startingTime
+
+let gameState =
+  GameState.READY
+
 let wins = 0
 let losses = 0
 let gamesPlayed = 0
 let bestTimeRemaining = null
 
-function moveFrog(e) {
-    const arrowKeys = ['ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown']
-
-    if (!timerId || gameOver || !arrowKeys.includes(e.key)) {
-        return
-    }
-
-    e.preventDefault()
-
-    squares[currentIndex].classList.remove('frog')
-
-    switch(e.key) {
-        case 'ArrowLeft' :
-             if (currentIndex % width !== 0) currentIndex -= 1
-            break
-        case 'ArrowRight' :
-            if (currentIndex % width < width - 1) currentIndex += 1
-            break
-        case 'ArrowUp' :
-            if (currentIndex - width >=0 ) currentIndex -= width
-            break
-        case 'ArrowDown' :
-            if (currentIndex + width < width * width) currentIndex += width
-            break
-    }
-    squares[currentIndex].classList.add('frog')
+function isRunning() {
+  return (
+    gameState ===
+    GameState.RUNNING
+  )
 }
 
-function autoMoveElements() {
-    const currentRow = Math.floor(currentIndex / width)
+function setGameState(state) {
+  gameState = state
 
-    const ridingLeftLog =
-        currentRow === 2 &&
-        (
-            squares[currentIndex].classList.contains('l1') ||
-            squares[currentIndex].classList.contains('l2') ||
-            squares[currentIndex].classList.contains('l3')
-        )
+  const statusByState = {
+    [GameState.READY]: 'Ready',
+    [GameState.RUNNING]: 'Running',
+    [GameState.PAUSED]: 'Paused',
+    [GameState.FINISHED]: 'Finished'
+  }
 
-    const ridingRightLog =
-        currentRow === 3 &&
-        (
-            squares[currentIndex].classList.contains('l1') ||
-            squares[currentIndex].classList.contains('l2') ||
-            squares[currentIndex].classList.contains('l3')
-        )
+  gameStatusDisplay.textContent =
+    statusByState[state]
+}
 
-    currentTime--
-    timeLeftDisplay.textContent = currentTime
+function moveFrogByKey(key) {
+  const arrowKeys = [
+    'ArrowLeft',
+    'ArrowRight',
+    'ArrowUp',
+    'ArrowDown'
+  ]
 
-    logsLeft.forEach(logLeft => moveLogLeft(logLeft))
-    logsRight.forEach(logRight => moveLogRight(logRight))
-    carsLeft.forEach(carLeft => moveCarLeft(carLeft))
-    carsRight.forEach(carRight => moveCarRight(carRight))
+  if (
+    !isRunning() ||
+    !arrowKeys.includes(key)
+  ) {
+    return
+  }
 
-    if (ridingLeftLog) {
-        rideLog(-1)
-    } else if (ridingRightLog) {
-        rideLog(1)
-    }
+  squares[currentIndex]
+    .classList.remove('frog')
+
+  switch (key) {
+    case 'ArrowLeft':
+      if (
+        currentIndex % width !== 0
+      ) {
+        currentIndex -= 1
+      }
+      break
+
+    case 'ArrowRight':
+      if (
+        currentIndex % width <
+        width - 1
+      ) {
+        currentIndex += 1
+      }
+      break
+
+    case 'ArrowUp':
+      if (
+        currentIndex - width >= 0
+      ) {
+        currentIndex -= width
+      }
+      break
+
+    case 'ArrowDown':
+      if (
+        currentIndex + width <
+        width * width
+      ) {
+        currentIndex += width
+      }
+      break
+  }
+
+  squares[currentIndex]
+    .classList.add('frog')
+}
+
+function handleFrogKey(event) {
+  const arrowKeys = [
+    'ArrowLeft',
+    'ArrowRight',
+    'ArrowUp',
+    'ArrowDown'
+  ]
+
+  if (
+    !isRunning() ||
+    !arrowKeys.includes(event.key)
+  ) {
+    return
+  }
+
+  event.preventDefault()
+  moveFrogByKey(event.key)
+}
+
+function advanceCycle(
+  element,
+  cycle,
+  direction
+) {
+  const currentIndex =
+    cycle.findIndex(className =>
+      element.classList.contains(
+        className
+      )
+    )
+
+  if (currentIndex < 0) {
+    return
+  }
+
+  const nextIndex =
+    (
+      currentIndex +
+      direction +
+      cycle.length
+    ) % cycle.length
+
+  element.classList.replace(
+    cycle[currentIndex],
+    cycle[nextIndex]
+  )
 }
 
 function rideLog(offset) {
-    const oldIndex = currentIndex
-    const nextIndex = currentIndex + offset
+  const oldIndex = currentIndex
+  const nextIndex =
+    currentIndex + offset
 
-    const oldRow = Math.floor(oldIndex / width)
-    const nextRow = Math.floor(nextIndex / width)
+  const oldRow =
+    Math.floor(oldIndex / width)
 
-    squares[oldIndex].classList.remove('frog')
+  const nextRow =
+    Math.floor(nextIndex / width)
 
-    if (
-        nextIndex < 0 ||
-        nextIndex >= squares.length ||
-        oldRow !== nextRow
-    ) {
-        finishGame('You lose!', false)
-        return
-    }
+  squares[oldIndex]
+    .classList.remove('frog')
 
-    currentIndex = nextIndex
-    squares[currentIndex].classList.add('frog')
+  if (
+    nextIndex < 0 ||
+    nextIndex >= squares.length ||
+    oldRow !== nextRow
+  ) {
+    finishGame(
+      'You lose!',
+      false
+    )
+
+    return
+  }
+
+  currentIndex = nextIndex
+
+  squares[currentIndex]
+    .classList.add('frog')
 }
 
-function checkOutComes() {
-    lose()
+function autoMoveElements() {
+  const currentRow =
+    Math.floor(
+      currentIndex / width
+    )
 
-    if (!gameOver) {
-        win()
-    }
-}
+  const ridingLeftLog =
+    currentRow === 2 &&
+    (
+      squares[currentIndex]
+        .classList.contains('l1') ||
+      squares[currentIndex]
+        .classList.contains('l2') ||
+      squares[currentIndex]
+        .classList.contains('l3')
+    )
 
-function moveLogLeft(logLeft) {
-    switch(true) {
-        case logLeft.classList.contains('l1') :
-            logLeft.classList.remove('l1')
-            logLeft.classList.add('l2')
-            break
-        case logLeft.classList.contains('l2') :
-            logLeft.classList.remove('l2')
-            logLeft.classList.add('l3')
-            break
-        case logLeft.classList.contains('l3') :
-            logLeft.classList.remove('l3')
-            logLeft.classList.add('l4')
-            break
-        case logLeft.classList.contains('l4') :
-            logLeft.classList.remove('l4')
-            logLeft.classList.add('l5')
-            break
-        case logLeft.classList.contains('l5') :
-            logLeft.classList.remove('l5')
-            logLeft.classList.add('l1')
-            break
-    }
-}
+  const ridingRightLog =
+    currentRow === 3 &&
+    (
+      squares[currentIndex]
+        .classList.contains('l1') ||
+      squares[currentIndex]
+        .classList.contains('l2') ||
+      squares[currentIndex]
+        .classList.contains('l3')
+    )
 
-function moveLogRight(logRight) {
-    switch(true) {
-        case logRight.classList.contains('l1') :
-            logRight.classList.remove('l1')
-            logRight.classList.add('l5')
-            break
-        case logRight.classList.contains('l2') :
-            logRight.classList.remove('l2')
-            logRight.classList.add('l1')
-            break
-        case logRight.classList.contains('l3') :
-            logRight.classList.remove('l3')
-            logRight.classList.add('l2')
-            break
-        case logRight.classList.contains('l4') :
-            logRight.classList.remove('l4')
-            logRight.classList.add('l3')
-            break
-        case logRight.classList.contains('l5') :
-            logRight.classList.remove('l5')
-            logRight.classList.add('l4')
-            break
-    }
-}
+  currentTime--
+  timeLeftDisplay.textContent =
+    currentTime
 
-function moveCarLeft(carLeft) {
-    switch(true) {
-        case carLeft.classList.contains('c1') :
-            carLeft.classList.remove('c1')
-            carLeft.classList.add('c2')
-            break
-        case carLeft.classList.contains('c2') :
-            carLeft.classList.remove('c2')
-            carLeft.classList.add('c3')
-            break
-        case carLeft.classList.contains('c3') :
-            carLeft.classList.remove('c3')
-            carLeft.classList.add('c1')
-            break
-    }
-}
+  logsLeft.forEach(element => {
+    advanceCycle(
+      element,
+      logCycle,
+      1
+    )
+  })
 
-function moveCarRight(carRight) {
-    switch(true) {
-        case carRight.classList.contains('c1') :
-            carRight.classList.remove('c1')
-            carRight.classList.add('c3')
-            break
-        case carRight.classList.contains('c2') :
-            carRight.classList.remove('c2')
-            carRight.classList.add('c1')
-            break
-        case carRight.classList.contains('c3') :
-            carRight.classList.remove('c3')
-            carRight.classList.add('c2')
-            break
-    }
-}
+  logsRight.forEach(element => {
+    advanceCycle(
+      element,
+      logCycle,
+      -1
+    )
+  })
 
-function resetGame() {
-    clearInterval(timerId)
-    clearInterval(outcomeTimerId)
+  carsLeft.forEach(element => {
+    advanceCycle(
+      element,
+      carCycle,
+      1
+    )
+  })
 
-    timerId = null
-    outcomeTimerId = null
+  carsRight.forEach(element => {
+    advanceCycle(
+      element,
+      carCycle,
+      -1
+    )
+  })
 
-    document.removeEventListener('keyup', moveFrog)
-
-    initialSquareClasses.forEach((className, index) => {
-        squares[index].className = className
-    })
-
-    currentIndex = startingIndex
-    currentTime = 20
-    gameOver = false
-
-    document.body.classList.remove('round-won', 'round-lost')
-
-    timeLeftDisplay.textContent = currentTime
-    resultDisplay.textContent = ''
-    startPauseButton.textContent = 'Start Game'
-    gameStatusDisplay.textContent = 'Ready'
-}
-
-function finishGame(message, didWin, removeFrog = false) {
-    if (gameOver) {
-        return
-    }
-
-    clearInterval(timerId)
-    clearInterval(outcomeTimerId)
-
-    timerId = null
-    outcomeTimerId = null
-    gameOver = true
-
-    document.removeEventListener('keyup', moveFrog)
-
-    if (removeFrog) {
-        squares[currentIndex].classList.remove('frog')
-    }
-
-    gamesPlayed++
-
-    if (didWin) {
-        wins++
-
-        if (
-            bestTimeRemaining === null ||
-            currentTime > bestTimeRemaining
-        ) {
-            bestTimeRemaining = currentTime
-            bestTimeDisplay.textContent = `${bestTimeRemaining}s`
-        }
-    } else {
-        losses++
-    }
-
-    winsDisplay.textContent = wins
-    lossesDisplay.textContent = losses
-    gamesPlayedDisplay.textContent = gamesPlayed
-
-    document.body.classList.remove('round-won', 'round-lost')
-    document.body.classList.add(didWin ? 'round-won' : 'round-lost')
-
-    resultDisplay.textContent = message
-    gameStatusDisplay.textContent = didWin ? 'Won' : 'Lost'
-    startPauseButton.textContent = 'Play Again'
+  if (ridingLeftLog) {
+    rideLog(-1)
+  } else if (ridingRightLog) {
+    rideLog(1)
+  }
 }
 
 function lose() {
-    if (
-        squares[currentIndex].classList.contains('c1') ||
-        squares[currentIndex].classList.contains('l4') ||
-        squares[currentIndex].classList.contains('l5') ||
-        currentTime <= 0
-    ) {
-        finishGame('You lose!', false, true)
-    }
+  if (
+    squares[currentIndex]
+      .classList.contains('c1') ||
+    squares[currentIndex]
+      .classList.contains('l4') ||
+    squares[currentIndex]
+      .classList.contains('l5') ||
+    currentTime <= 0
+  ) {
+    finishGame(
+      'You lose!',
+      false,
+      true
+    )
+  }
 }
 
 function win() {
-    if (squares[currentIndex].classList.contains('ending-block')) {
-        finishGame('You Win!', true)
+  if (
+    squares[currentIndex]
+      .classList.contains(
+        'ending-block'
+      )
+  ) {
+    finishGame(
+      'You Win!',
+      true
+    )
+  }
+}
+
+function checkOutcomes() {
+  lose()
+
+  if (
+    gameState !==
+    GameState.FINISHED
+  ) {
+    win()
+  }
+}
+
+function restoreBoard() {
+  initialSquareClasses.forEach(
+    (className, index) => {
+      squares[index].className =
+        className
     }
+  )
+}
+
+function resetGame() {
+  timers.clearAll()
+
+  document.removeEventListener(
+    'keyup',
+    handleFrogKey
+  )
+
+  restoreBoard()
+
+  currentIndex = startingIndex
+  currentTime = startingTime
+
+  document.body.classList.remove(
+    'round-won',
+    'round-lost'
+  )
+
+  timeLeftDisplay.textContent =
+    currentTime
+
+  resultDisplay.textContent = ''
+
+  startPauseButton.textContent =
+    'Start Game'
+
+  setGameState(
+    GameState.READY
+  )
+}
+
+function finishGame(
+  message,
+  didWin,
+  removeFrog = false
+) {
+  if (
+    gameState ===
+    GameState.FINISHED
+  ) {
+    return
+  }
+
+  timers.clearAll()
+
+  document.removeEventListener(
+    'keyup',
+    handleFrogKey
+  )
+
+  if (removeFrog) {
+    squares[currentIndex]
+      .classList.remove('frog')
+  }
+
+  gamesPlayed++
+
+  if (didWin) {
+    wins++
+
+    if (
+      bestTimeRemaining === null ||
+      currentTime >
+      bestTimeRemaining
+    ) {
+      bestTimeRemaining =
+        currentTime
+
+      bestTimeDisplay.textContent =
+        `${bestTimeRemaining}s`
+    }
+  } else {
+    losses++
+  }
+
+  winsDisplay.textContent = wins
+  lossesDisplay.textContent = losses
+  gamesPlayedDisplay.textContent =
+    gamesPlayed
+
+  document.body.classList.remove(
+    'round-won',
+    'round-lost'
+  )
+
+  document.body.classList.add(
+    didWin
+      ? 'round-won'
+      : 'round-lost'
+  )
+
+  resultDisplay.textContent =
+    message
+
+  setGameState(
+    GameState.FINISHED
+  )
+
+  gameStatusDisplay.textContent =
+    didWin ? 'Won' : 'Lost'
+
+  startPauseButton.textContent =
+    'Play Again'
 }
 
 function startGame() {
-    timerId = setInterval(autoMoveElements, 1000)
-    outcomeTimerId = setInterval(checkOutComes, 50)
+  if (isRunning()) {
+    return
+  }
 
-    document.addEventListener('keyup', moveFrog)
+  timers.interval(
+    autoMoveElements,
+    1000
+  )
 
-    startPauseButton.textContent = 'Pause Game'
-    gameStatusDisplay.textContent = 'Running'
+  timers.interval(
+    checkOutcomes,
+    50
+  )
+
+  document.addEventListener(
+    'keyup',
+    handleFrogKey
+  )
+
+  startPauseButton.textContent =
+    'Pause Game'
+
+  setGameState(
+    GameState.RUNNING
+  )
 }
 
 function pauseGame() {
-    clearInterval(timerId)
-    clearInterval(outcomeTimerId)
+  if (!isRunning()) {
+    return
+  }
 
-    timerId = null
-    outcomeTimerId = null
+  timers.clearAll()
 
-    document.removeEventListener('keyup', moveFrog)
+  document.removeEventListener(
+    'keyup',
+    handleFrogKey
+  )
 
-    startPauseButton.textContent = 'Resume Game'
-    gameStatusDisplay.textContent = 'Paused'
+  startPauseButton.textContent =
+    'Resume Game'
+
+  setGameState(
+    GameState.PAUSED
+  )
 }
 
-startPauseButton.addEventListener('click', () => {
-    if (gameOver) {
-        resetGame()
-        startGame()
-        return
+startPauseButton.addEventListener(
+  'click',
+  () => {
+    if (
+      gameState ===
+      GameState.FINISHED
+    ) {
+      resetGame()
+      startGame()
+      return
     }
 
-    if (timerId) {
-        pauseGame()
+    if (isRunning()) {
+      pauseGame()
     } else {
-        startGame()
+      startGame()
     }
-})
-resetSessionButton.addEventListener('click', () => {
+  }
+)
+
+resetSessionButton.addEventListener(
+  'click',
+  () => {
     resetGame()
 
     wins = 0
@@ -336,23 +564,25 @@ resetSessionButton.addEventListener('click', () => {
 
     winsDisplay.textContent = wins
     lossesDisplay.textContent = losses
-    gamesPlayedDisplay.textContent = gamesPlayed
-    bestTimeDisplay.textContent = '--'
+    gamesPlayedDisplay.textContent =
+      gamesPlayed
 
-    resultDisplay.textContent = ''
-    gameStatusDisplay.textContent = 'Ready'
-})
+    bestTimeDisplay.textContent =
+      '--'
+  }
+)
 
-document.querySelectorAll('.move-button').forEach(button => {
-    button.addEventListener('click', () => {
-        if (!timerId || gameOver) {
-            return
-        }
-
-        document.dispatchEvent(
-            new KeyboardEvent('keyup', {
-                key: button.dataset.key
-            })
+document
+  .querySelectorAll('.move-button')
+  .forEach(button => {
+    button.addEventListener(
+      'click',
+      () => {
+        moveFrogByKey(
+          button.dataset.key
         )
-    })
-})
+      }
+    )
+  })
+
+resetGame()
